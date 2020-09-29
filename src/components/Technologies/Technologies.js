3849df6ce0088234fa0762aad6e4f750d8d95eba
Ofir Levi <ofirl24@gmail.com>
2020-09-29 19:17:20 +0300

    more animations

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,7 +1,9 @@
-import { Button, makeStyles } from '@material-ui/core';
+import { Button, makeStyles, Typography } from '@material-ui/core';
 import clsx from 'clsx';
 import React, { useState } from 'react';
+import { animated, useTransition } from 'react-spring';
 import { Cell, Grid } from 'styled-css-grid';
+import { animationSlowSpringConfig } from '../../utils/animationUtils';
 
 const useStyles = makeStyles(theme => ({
     backgroundDiv: {
@@ -58,6 +60,13 @@ const useStyles = makeStyles(theme => ({
         maxWidth: '10em',
         opacity: '1',
     },
+    nodeDescriptionGrid: {
+        paddingTop: '1em',
+        paddingRight: '2em',
+        paddingLeft: '2em',
+        textAlign: 'center',
+        color: 'white',
+    },
 }));
 
 const Node = ({ image, title, selected, onClick }) => {
@@ -80,20 +89,49 @@ const Technologies = () => {
         {
             title: 'React',
             image: "/assets/images/logos/react.png",
+            description: [
+                'description very very very very very very very very very very very very very very very very very very very long line 1',
+                null,
+                'description line 2'
+            ]
         },
         {
             title: 'NodeJS',
             image: "/assets/images/logos/nodejs.png",
+            description: [
+                'description very very very very very very very very very very very very very very very very very very very long line 1',
+                'description line 2'
+            ]
         },
         {
             title: 'Express',
             image: "/assets/images/logos/express.png",
+            description: [
+                'description very very very very very very very very very very very very very very very very very very very long line 1',
+                'description line 2'
+            ]
         },
         {
             title: 'MongoDB',
             image: "/assets/images/logos/mongo.png",
+            description: [
+                'description very very very very very very very very very very very very very very very very very very very long line 1',
+                'description line 2'
+            ]
         }
     ];
+
+    let nodeDescriptionTransitions = useTransition(selectedNode, null, {
+        from: {
+            opacity: 0,
+        },
+        enter: {
+            opacity: 1
+        },
+        leave: {
+            opacity: 0
+        },
+    });
 
     const handleNodeClick = (idx) => {
         setSelectedNode(idx);
@@ -102,13 +140,44 @@ const Technologies = () => {
     return (
         <>
             <div className={classes.backgroundDiv} />
-            <Grid gap="0" columns="1fr" rows="auto auto 1fr" areas={["nodes description projects"]}>
+            <Grid gap="0" columns="1fr" rows="auto auto 1fr" areas={["nodes", "description", "projects"]}>
                 <Cell area="nodes">
                     <Grid className={classes.nodesGrid} gap="0" columns="1fr repeat(4, auto) 1fr" rows="1fr" areas={[". React NodeJS Express MongoDB"]}>
                         {
                             nodes.map((n, idx) => (
                                 <Cell key={idx} area={n.title}>
                                     <Node selected={selectedNode === idx} title={n.title} image={n.image} onClick={() => handleNodeClick(idx)} />
+                                </Cell>
+                            ))
+                        }
+                    </Grid>
+                </Cell>
+                <Cell area="description">
+                    <Grid gap="0" columns="1fr" rows="auto 1fr" className={classes.nodeDescriptionGrid} areas={["title", "description"]}>
+                        {
+                            nodeDescriptionTransitions.map(({ item, key, props }) => (
+                                <Cell className="posRelative" area="title">
+                                    <animated.div style={props} className="test">
+                                        <Typography variant="h6">
+                                            {nodes[item].title}
+                                        </Typography>
+                                    </animated.div>
+                                </Cell>
+                            ))
+                        }
+                        {
+                            nodeDescriptionTransitions.map(({ item, key, props }) => (
+                                <Cell className="posRelative" area="description">
+                                    <animated.div style={props} className="test">
+                                        {
+                                            nodes[item].description.map(t =>
+                                                <Typography variant="body1">
+                                                    {/* not a space!!! (altCode 0160) */}
+                                                    {t ? t : ' '}
+                                                </Typography>
+                                            )
+                                        }
+                                    </animated.div>
                                 </Cell>
                             ))
                         }
