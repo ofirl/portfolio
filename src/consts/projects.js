69b68c76f6575c0963559d013717a220c7d15006
Ofir Levi <ofirl24@gmail.com>
2020-10-06 18:07:31 +0300

    changed text of shopndine

--- src/consts/projects.js
+++ src/consts/projects.js
@@ -12,30 +12,32 @@ export const projects = [
                 type: 'Text',
                 highlight: highlights.green,
                 value: [
-                    'ShopNDine is a map utility for "Hever" that aims to bring the simplicity of a map view to the community.',
-                    'It is a free web application that shows all the buisnesses that accept any kind of "Hever" cards on a map.'
+                    'ShopNDine is a map utility intended for "Hever" club members. The application’s vision is to create a better way to explore the club’s benefits.',
+                    'It is a free web application that shows all the locations of businesses that collaborate with the “Hever” club and accept any kind of "Hever" card.'
                 ]
             },
             {
                 type: 'Text',
                 highlight: highlights.green,
-                value: 'The purpose of ShopNDine was to make it easier to find relevant businesses with the help of a map view, filters, and the use of location services.',
+                value: 'ShopNDine was made for the purpose of making a convenient way to find relevant businesses with the help of a map view, filters, and the use of location services.',
             },
             {
                 type: 'Text',
                 highlight: highlights.green,
                 value: [
-                    'It was put behind an email whitelist and password after "Hever" changed their terms of use to disallow such applications from existing, and remained in personal use only.',
-                    'If you wish to see it and check it out email me and ask for a password (all passwords have an expiration date to prevent misuse).'
+                    'Due to changes in “Hever” club’s terms of use, which prohibit the existence of such applications, an email whitelist and a password was added to ShopNDine.',
+                    'I encourage you to email me and ask for a password in order for you to see and to experience with the application.'
                 ]
             },
             {
                 type: 'Header',
-                value: 'What I Did'
+                value: 'My Contribution'
             },
             {
                 type: 'Text',
-                value: 'I designed the application from the ground up based on my own vision as a member of "Hever" (visual design was made to copy google maps for ease of use and sense of familiarity).'
+                value: `The application was made by me from scratch, based on my own vision as a member of "Hever" club.
+                The idea, the design, the architecture of the database, the collection of the requirements were all my doing. 
+                The visual design was made with Google Maps in mind for ease of use and a sense of familiarity.`
             },
             {
                 type: 'Header',
@@ -44,60 +46,36 @@ export const projects = [
             {
                 type: 'Text',
                 value: [
-                    `The server is running cron jobs to update the database with new data every week since update takes about 3 hours, 
-                    i'm checking each location data for consistency with OpenStreetMaps.`
+                    `The server is running cron jobs every week in order to update the database with new data. 
+                    In each update, there is a consistency check for each location with OpenStreetMaps.`
                 ],
             },
             {
                 type: 'Text',
                 value: [
-                    'One of my requirments was for the application to be performant and use as little bandwith as possible.',
-                    'I made the decision of saving the critical data (business name, location and other basic data) on the users device (using local storage), this made the way to make the application a PWA (Progressive Web App) very easy.'
+                    'One of the requirements I set up for the application was its performance, particularly use as little bandwidth as possible.',
+                    'I made the decision of saving the critical data - business name, location and other basic data - on the user’s device (using local storage), which made it easy to make the application a PWA (Progressive Web App).'
                 ]
             },
             {
                 type: 'Text',
                 value: [
-                    `In order to be efficient i also implemented delta updates, 
-                    The application checks for an update every time it starts and if its available the server brings only the data that was changed since the last update.`
+                    `In order to be efficient I also implemented delta updates.  
+                    The application checks for an update every time it starts and if there is, the server extracts only the data that was changed since the last update check.`
                 ]
             },
-            {
-                type: 'List',
-                // highlight: highlights.green,
-                value: [
-                    'bullet1',
-                    'bullet2',
-                    'bullet3',
-                    'bullet4',
-                    'bullet5',
-                    'bullet6',
-                ]
-            },
-            {
-                type: 'List',
-                // highlight: highlights.green,
-                value: [
-                    'bullet1',
-                    'bullet2',
-                    'bullet3',
-                    'bullet4',
-                    'bullet5',
-                    'bullet6',
-                ]
-            },
-            {
-                type: 'List',
-                // highlight: highlights.green,
-                value: [
-                    'bullet1',
-                    'bullet2',
-                    'bullet3',
-                    'bullet4',
-                    'bullet5',
-                    'bullet6',
-                ]
-            },
+            // {
+            //     type: 'List',
+            //     // highlight: highlights.green,
+            //     value: [
+            //         'bullet1',
+            //         'bullet2',
+            //         'bullet3',
+            //         'bullet4',
+            //         'bullet5',
+            //         'bullet6',
+            //     ]
+            // },
         ],
     },
     {
